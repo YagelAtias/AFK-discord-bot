bb9b710d86a0fc13506554ffc1613faa3ce6dba1
YagelAtias <yagelatias@outlook.com>
2025-09-16 19:20:27 +0300

    add AFK threshold and bot intents

--- main.py
+++ main.py
@@ -3,7 +3,14 @@ from dotenv import load_dotenv
 import discord
 from discord.ext import tasks, commands
 import datetime
+
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 CHANNEL_ID = int(os.getenv('CHANNEL_ID'))
 USER_ID = int(os.getenv("USER_ID"))
+
+AWAY_ALERT_THRESHOLD_MINUTES = 15
+intents = discord.Intents.default()
+intents.members = True
+intents.presences = True
+intents.voice_states = True
