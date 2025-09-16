32b1cdfeff90acae1e0d048d1ce2d37cdf545d2c
YagelAtias <yagelatias@outlook.com>
2025-09-16 19:38:44 +0300

    feat(voice): initial voice state: track activity

--- main.py
+++ main.py
@@ -14,3 +14,14 @@ intents = discord.Intents.default()
 intents.members = True
 intents.presences = True
 intents.voice_states = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+last_active = {}
+
+# This event happens whenever the voice state changes on discord.
+@bot.event
+async def on_voice_state_update(member, before, after):
+    if member.id == USER_ID:
+        if before.channel != after.channel or before.self_mute != after.self_mute or before.self_deaf != after.self_deaf:
+            last_active[USER_ID] = datetime.datetime.now()
